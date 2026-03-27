a2690500364c0255c73f5bfbffa2c8846f858dac
concordia35 <oddfellow.concordia35@gmail.com>
2026-03-27 16:39:32 +0100

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://qqxotyrhaqvkotcghpcd.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_llg2yTp7OcB2FO1zOqBeRA_cC6NUmcb";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
