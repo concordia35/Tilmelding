f4c55274f74bb34b5181555a06af599806118cd9
concordia35 <oddfellow.concordia35@gmail.com>
2026-03-27 17:11:17 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://qqxotyrhaqvkotcghpcd.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_llg2yTp7OcB2FO1zOqBeRA_cC6NUmcb";
+export const SUPABASE_URL = "https://qqxotyrhaqvkotcghpcd.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_llg2yTp7OcB2FO1zOqBeRA_cC6NUmcb";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
